8a5e3473e8342f30b6a037481e34c28e2db3b1c8
Estevan Fernandes <estevan.stolfo05@gmail.com>
2025-09-12 01:23:09 -0300

    Ajustes no Greeting Service

--- greeting-service/src/main/java/br/edu/atitus/greeting_service/controllers/GreetingController.java
+++ greeting-service/src/main/java/br/edu/atitus/greeting_service/controllers/GreetingController.java
@@ -1,38 +1,32 @@
 package br.edu.atitus.greeting_service.controllers;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import br.edu.atitus.greeting_service.configs.GreetingConfig;
+import java.util.Map;
 
 @RestController
-@RequestMapping("greeting")
 public class GreetingController {
-	
-	//@Value("${greeting-service.greeting}")
-	//private String greeting;
-	//@Value("${greeting-service.default-name}")
-	//private String defaultName;
-	
-	private final GreetingConfig config;
-	
-	public GreetingController(GreetingConfig config) {
-		super();
-		this.config = config;
-	}
 
-	@GetMapping
-	public ResponseEntity<String> greet(
-			@RequestParam(required = false) String name){
-		String greetingReturn = config.getGreeting();
-		String nameReturn = name != null ? name : config.getDefaultName();
-		String textReturn = String.format("%s, %s!!!", greetingReturn, nameReturn);
-		
-		return ResponseEntity.ok(textReturn);
-	}
+    @Value("${greeting-service.greeting}")
+    private String greeting;
 
+    @Value("${greeting-service.default-name}")
+    private String defaultName;
+
+    @GetMapping("/greeting/{name}")
+    public String greeting(@PathVariable String name) {
+        return String.format("%s, %s!!!", greeting, name);
+    }
+
+    @GetMapping("/greeting")
+    public String greetingDefault() {
+        return String.format("%s, %s!!!", greeting, defaultName);
+    }
+
+    @PostMapping("/greeting")
+    public String greetingPost(@RequestBody Map<String, String> body) {
+        String name = body.getOrDefault("name", defaultName);
+        return String.format("%s, %s!!!", greeting, name);
+    }
 }
