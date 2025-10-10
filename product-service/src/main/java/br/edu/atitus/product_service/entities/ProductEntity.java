16a04893a463f7935e4f084c5b358140476ec015
Estevan Fernandes <estevan.stolfo05@gmail.com>
2025-10-10 18:00:44 -0300

    product-service atualizado

--- product-service/src/main/java/br/edu/atitus/product_service/entities/ProductEntity.java
+++ product-service/src/main/java/br/edu/atitus/product_service/entities/ProductEntity.java
@@ -1,8 +1,12 @@
 package br.edu.atitus.product_service.entities;
 
-import jakarta.persistence.*;
-
-import java.math.BigDecimal;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "tb_product")
@@ -12,25 +16,75 @@ public class ProductEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
-    private String name;
-
-    @Column(nullable = false)
-    private BigDecimal price;
-
-    @Column(nullable = false)
+    private String description;
+    private String brand;
+    private String model;
+    private double price;
+    @Column(length = 3)
+    private String currency;
     private Integer stock;
 
-    // Getters e Setters
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+    @Transient
+    private String enviroment;
+    @Transient
+    private double convertedPrice;
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public String getBrand() {
+        return brand;
+    }
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+    public String getModel() {
+        return model;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public String getCurrency() {
+        return currency;
+    }
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+    public Integer getStock() {
+        return stock;
+    }
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+    public String getEnviroment() {
+        return enviroment;
+    }
+    public void setEnviroment(String enviroment) {
+        this.enviroment = enviroment;
+    }
+    public double getConvertedPrice() {
+        return convertedPrice;
+    }
+    public void setConvertedPrice(double convertedPrice) {
+        this.convertedPrice = convertedPrice;
+    }
 
-    public BigDecimal getPrice() { return price; }
-    public void setPrice(BigDecimal price) { this.price = price; }
 
-    public Integer getStock() { return stock; }
-    public void setStock(Integer stock) { this.stock = stock; }
+
+
 }
--- product-service/src/main/java/br/edu/atitus/product_service/repositories/ProductRepository.java
+++ product-service/src/main/java/br/edu/atitus/product_service/repositories/ProductRepository.java
@@ -1,11 +1,11 @@
 package br.edu.atitus.product_service.repositories;
 
-import br.edu.atitus.product_service.entities.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.List;
+
+import br.edu.atitus.product_service.entities.ProductEntity;
 
 @Repository
-public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
-    List<ProductEntity> findByNameContainingIgnoreCase(String name);
+public interface ProductRepository extends JpaRepository<ProductEntity, Long>{
+
 }
